da01c8acc7feefa4683413ef279e1d0ec7006651
nathan ks <nathanks0210@gmail.com>
2024-08-05 23:07:36 -0300

    feat: Add sign-up route and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/view/home/home.component';
 import { ListingComponent } from './components/view/listing/listing.component';
 import { NotFoundComponent } from './components/view/not-found/not-found.component';
+import { SignUpComponent } from './components/view/sign-up/sign-up.component';
 
 export const routes: Routes = [
   {
@@ -25,6 +26,12 @@ export const routes: Routes = [
   {
     path: 'produtos/:categoria',
     component: ListingComponent,
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'usuario/cadastro',
+    component: SignUpComponent,
     pathMatch: 'full',
   },
 
