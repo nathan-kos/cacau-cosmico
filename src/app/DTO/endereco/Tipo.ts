1da1db598a496ad531c360a19ca495016233ce6d
nathan ks <nathanks0210@gmail.com>
2024-08-12 22:00:20 -0300

    feat: Add DTO classes for Endereco, Tipo, and UF

---
+++ src/app/DTO/endereco/Tipo.ts
@@ -0,0 +1,10 @@
+enum Tipo {
+  RESIDENCIAL = 'Residencial',
+  COMERCIAL = 'Comercial',
+  OUTRO = 'Outro',
+  ENTREGA = 'Entrega',
+  COBRANCA = 'Cobrança',
+  CONDOMINIO = 'Condomínio',
+}
+
+export { Tipo };
