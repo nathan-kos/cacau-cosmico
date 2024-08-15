d53c2685c11c301eb8f4d082b0899acb19a7915d
nathan ks <nathanks0210@gmail.com>
2024-08-15 20:18:51 -0300

    refactor: Update confirmation component to include delete functionality

--- src/app/components/view/list-users/list-users.component.ts
+++ src/app/components/view/list-users/list-users.component.ts
@@ -1,12 +1,13 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { Usuario } from '../../../DTO/Usuario/Usuario';
+import { ConfirmacaoComponent } from '../../resources/confirmacao/confirmacao.component';
 import { HeaderComponent } from '../../resources/header/header.component';
 
 @Component({
   selector: 'app-list-users',
   standalone: true,
-  imports: [HeaderComponent, NgFor],
+  imports: [HeaderComponent, NgFor, ConfirmacaoComponent, NgIf],
   templateUrl: './list-users.component.html',
   styleUrl: './list-users.component.css',
 })
@@ -26,5 +27,39 @@ export class ListUsersComponent {
       usu_Senha: '123123',
       usu_Telefone: '123123123',
     },
+    {
+      usu_Id: '2',
+      usu_Nome: 'Maria',
+      usu_Email: 'jkhdsjhkd',
+      usu_Ativo: true,
+      usu_CPF: '123123123',
+      usu_CriadoEm: '2021-07-07',
+      usu_AtualizadoEm: '2021-07-07',
+      usu_Genero: 'F',
+      usu_Nasc: '1999-07-07',
+      usu_pap: '1',
+      usu_Senha: '123123',
+      usu_Telefone: '123123123',
+    },
   ];
+
+  public selectedUser: Usuario | null = null;
+
+  public showDeleteModal = false;
+  public showEditModal = false;
+
+  public openDeleteModal(user: Usuario) {
+    this.selectedUser = user;
+    this.showDeleteModal = true;
+  }
+
+  public closeDeleteModal() {
+    this.showDeleteModal = false;
+  }
+
+  public deleteUser() {
+    this.showDeleteModal = false;
+    // chama o backend para deletar o usuário
+    window.alert('Usuário deletado com sucesso');
+  }
 }
