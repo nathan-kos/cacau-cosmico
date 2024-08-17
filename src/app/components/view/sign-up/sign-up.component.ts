8492e83e95bd143972bbbc07b5de3584660d425c
nathan ks <nathanks0210@gmail.com>
2024-08-17 00:27:27 -0300

    refactor: Update sign-up component styles and form layout

--- src/app/components/view/sign-up/sign-up.component.ts
+++ src/app/components/view/sign-up/sign-up.component.ts
@@ -56,28 +56,28 @@ export class SignUpComponent {
   public onSubmit(): void {
     if (!this.signUpForm.valid) {
       // passa um por um e verifica se está válido e soma na mensagem de erro para exibir
-      this.error = 'Formulário inválido';
+      this.error = 'Formulário inválido<br>';
       if (this.signUpForm.controls['nome'].invalid) {
-        this.error += '\nNome inválido';
+        this.error += 'Nome inválido<br>';
       }
       if (this.signUpForm.controls['email'].invalid) {
-        this.error += '\nEmail inválido';
+        this.error += 'Email inválido<br>';
       }
       if (this.signUpForm.controls['senha'].invalid) {
-        this.error += '\nSenha inválida';
+        this.error += 'Senha inválida<br>';
       }
       if (this.signUpForm.controls['cpf'].invalid) {
-        this.error += '\nCPF inválido';
-        this.error += '\nCPF deve conter 11 dígitos';
+        this.error += 'CPF inválido<br>';
+        this.error += ' CPF deve conter 11 dígitos<br>';
       }
       if (this.signUpForm.controls['dataNascimento'].invalid) {
-        this.error += '\nData de nascimento inválida';
+        this.error += 'Data de nascimento inválida<br>';
       }
       if (this.signUpForm.controls['genero'].invalid) {
-        this.error += '\nGênero inválido';
+        this.error += 'Gênero inválido<br>';
       }
       if (this.signUpForm.controls['telefone'].invalid) {
-        this.error += '\nTelefone inválido';
+        this.error += 'Telefone inválido<br>';
       }
       return;
     }
