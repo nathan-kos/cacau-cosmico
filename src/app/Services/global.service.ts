7d656945dbc32f7dabe0c50b4dc04a15953b7b79
nathan ks <nathanks0210@gmail.com>
2024-08-29 19:30:59 -0300

    feat: Add defaultUsu_Id to global.service.ts

--- src/app/Services/global.service.ts
+++ src/app/Services/global.service.ts
@@ -5,4 +5,5 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalService {
   public baseUrl: string = 'http://localhost:3333/';
+  public defaultUsu_Id: string = '2f206edd-f69f-4bc6-a56f-5dbaa045e6b5';
 }
