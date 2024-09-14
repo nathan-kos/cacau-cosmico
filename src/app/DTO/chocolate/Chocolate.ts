6265433bd17c05901ffba75c2405f82764018558
nathan ks <nathanks0210@gmail.com>
2024-09-13 22:49:23 -0300

    feat: Add Chocolate class for chocolate details

---
+++ src/app/DTO/chocolate/Chocolate.ts
@@ -0,0 +1,41 @@
+import { Categorias } from './categorias';
+
+class Chocolate {
+  cho_Id: string;
+  cho_Nome: string;
+  cho_Descricao: string;
+  cho_Valor: number;
+  cho_Imagem: string;
+  cho_Ativo: boolean;
+  cho_Peso: number;
+  catergorias: Categorias[];
+
+  cho_AtualizadoEm: Date;
+  cho_CriadoEm: Date;
+
+  constructor(
+    cho_Id: string,
+    cho_Nome: string,
+    cho_Descricao: string,
+    cho_Valor: number,
+    cho_Imagem: string,
+    cho_Ativo: boolean,
+    cho_Peso: number,
+    catergorias: Categorias[],
+    cho_AtualizadoEm: Date,
+    cho_CriadoEm: Date
+  ) {
+    this.cho_Id = cho_Id;
+    this.cho_Nome = cho_Nome;
+    this.cho_Descricao = cho_Descricao;
+    this.cho_Valor = cho_Valor;
+    this.cho_Imagem = cho_Imagem;
+    this.cho_Ativo = cho_Ativo;
+    this.cho_Peso = cho_Peso;
+    this.catergorias = catergorias;
+    this.cho_AtualizadoEm = cho_AtualizadoEm;
+    this.cho_CriadoEm = cho_CriadoEm;
+  }
+}
+
+export { Chocolate };
