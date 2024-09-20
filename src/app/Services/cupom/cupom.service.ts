79d02938251f6f4f71deb2076e2ba16d8ee50d25
nathan ks <nathanks0210@gmail.com>
2024-09-19 22:34:29 -0300

    feat: Add CupomService and CupomServiceSpec
    - Add CupomService class to handle cupom-related functionality
    - Add CupomServiceSpec to test the functionality of CupomService

---
+++ src/app/Services/cupom/cupom.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { cupom } from '../../DTO/Cupom/cupom';
+import { ErrorDTO } from '../../DTO/Error/ErrorDTO';
+import { GlobalService } from '../global.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CupomService {
+  constructor(private http: HttpClient, private globalService: GlobalService) {}
+
+  public async FindByCodigo(codigo: string): Promise<cupom | ErrorDTO> {
+    try {
+      const response = await firstValueFrom(
+        this.http.get<any>(`${this.globalService.baseUrl}cupom/${codigo}`)
+      );
+
+      return response;
+    } catch (error) {
+      if (error instanceof HttpErrorResponse) {
+        return new ErrorDTO(error.error.message, error.status);
+      }
+      return new ErrorDTO('Erro desconhecido', 500);
+    }
+  }
+}
